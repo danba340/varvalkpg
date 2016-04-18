79ced1c7076560c2d04db9c50a17a636dfdd2997
danba340 <danielbark@Daniels-MacBook-Pro.local>
2016-04-18 23:16:02 +0200

    styling and ajax

--- js/main.js
+++ js/main.js
@@ -6,23 +6,33 @@ $(document).ready(function() {
     var numOfRunners = 100;
     var parentDiv = $("#container");
     for(var i = 1; i <= numOfRunners; i++){
-        parentDiv.append("<div id='button" + i + "' class='button'>" + i + "<div id='lapsRunnerNo" + i + "' class='lapcount'><span>Varv:</span>0</div></div>");
+        parentDiv.append("<div id='button" + i + "' class='button btn-success' data-runner-no='" + i +"'>Nr: " + i +"<div class='lapcount'><span>Varv: </span>0</div><button class='add btn-default'></button></div>");
     }
-    $('.button').on("click", function(){
-        var noEnlarged = true;
-        $('button').each(function(index, button){
-            if(button.hasClass("enlarge")){
-              noEnlarged = false;
+    $('.button').click(function(){
+        $(this).toggleClass("enlarge");
+
+    });
+    $('.add').click(function(){
+
+        $.ajax({
+            type:"GET"
+            , url:"http://vfv-api.azurewebsites.net/api/lap/add?startNumber=" + $(this).parent().data("runnerNo")
+            , dataType:"jsonp",
+            jsonpCallback: "localJsonpCallback",
+             success: function(data){
+                debugger;
+
+                console.log(data);
+
             }
+           , error: function(e) {
+               console.log(e)
+
+               }
         });
-        if(noEnlarged){
-          $(this).addClass("enlarge");
-        }
-        parentDiv.append("<div class='close'>X</div>");
+
     });
-    $('.close').on("click", function(){
-        $('.close').find("button"),removeClass("enlarge");
-    });
+
 });
 
 
