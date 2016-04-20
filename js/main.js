b532588d1c3ff72502c23eb9e530a2fa8073af87
danba340 <danielbark@dhcp166.ateles.se>
2016-04-20 08:30:07 +0200

    Fixed anchor to top

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
     var numOfRunners = 100;
     var parentDiv = $("#container");
     for(var i = 1; i <= numOfRunners; i++){
-        parentDiv.append("<div id='button" + i + "' class='button btn-success' data-runner-no='" + i +"'>Nr: " + i +"<div class='lapcount'><span>Varv: </span>0</div><button class='add btn-default'><div>+</div></button><button class='remove btn-default'><div>-</div></button></div>");
+        parentDiv.append("<a href='#top' ><div id='button" + i + "' class='button btn-success' data-runner-no='" + i +"'>Nr: " + i +"<div class='lapcount'><span>Varv: </span>0</div><button class='add btn-default'><div>+</div></button><button class='remove btn-default'><div>-</div></button></div></a>");
     }
     $('.button').click(function(){
         $(this).toggleClass("enlarge");
