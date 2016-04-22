eee34ba62a9c581deecc4c3edc1c19caf6a703d0
danba340 <danielbark@Daniels-MacBook-Pro.local>
2016-04-22 21:35:42 +0200

    Fixed rendering of all buttons at document ready with values from api

--- js/main.js
+++ js/main.js
@@ -2,19 +2,7 @@ $(function() {
     FastClick.attach(document.body);
 });
 
-function updateLaps(){
-  $('.button').each(function(){
-
-  });
-}
-
-$(document).ready(function() {
-    var numOfRunners = 100;
-    var parentDiv = $("#container");
-    var buttonID;
-    for(var i = 1; i <= numOfRunners; i++){
-        parentDiv.append("<a href='#top' ><div id='button" + i + "' class='button btn-success' data-runner-no='" + i +"'>Nr: " + i +"<div class='lapcount'><span>Varv: </span>0</div><button class='add btn-default'><div>+</div></button><button class='remove btn-default'><div>-</div></button></div></a>");
-    }
+function buttonsLoaded(){
     $('.button').click(function(){
         $(this).toggleClass("enlarge");
 
@@ -33,7 +21,7 @@ $(document).ready(function() {
     $('.remove').click(function(){
         var removeButton = $(this);
         $.get("http://vfv-api.azurewebsites.net/api/participant/removelap?startnumber=" + removeButton.parent().data("runnerNo"), function(data) {
-            var laphtml = "<span>Varv: </span>" + data.count;
+            var laphtml = "<span>Varv: </span>" + String(data.count - 1);
 
             removeButton.siblings(".lapcount").html(laphtml)
             console.log(data);
@@ -41,7 +29,16 @@ $(document).ready(function() {
         });
 
     });
-    updateLaps();
+}
+
+$(document).ready(function() {
+    $.get("http://vfv-api.azurewebsites.net/api/lap/getparticipantslapcount", function(data) {
+        $.each(data, function(key, val){
+            $("#container").append("<a href='#top' ><div id='button" + key + "' class='button btn-success' data-runner-no='" + key +"'>Nr: " + key +"<div class='lapcount'><span>Varv: </span>" + val +"</div><button class='add btn-default'><div>+</div></button><button class='remove btn-default'><div>-</div></button></div></a>");
+        });
+        buttonsLoaded();
+    });
+
 });
 
 
